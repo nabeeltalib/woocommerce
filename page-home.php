fde6246d04d18c2e84b90b07cfeda48e49451171
Nabeel Talib <engrnabeel.talib@gmial.com>
2022-06-20 16:15:24 +0500

    Changed page-home.php

--- page-home.php
+++ page-home.php
@@ -63,4 +63,16 @@
     </div>
 </section>
 
+<section class="main-banner-3 bg-primary">
+    <div class="container">
+        <div class="row text-center">
+            <div class="col-12 my-xxxl-11 my-xxl-10 my-sm-9 my-8">
+                <h1 class="text-white text-uppercase lh-1 display-1 fw-bold">Woocommerce</h1>
+                <button type="button" class="btn btn-light text-uppercase arrow mt-xxl-5 mt-3">Read More</button>
+            </div>
+        </div>
+    </div>
+</section>
+
+
 <?php get_footer(); ?>
